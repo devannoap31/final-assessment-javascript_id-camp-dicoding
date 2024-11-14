3dfb78f34e6960e882fc06f0167a3f2d5e5f39c1
devannoap31 <devannoap318@gmail.com>
2024-11-14 21:14:28 +0700

    solve OOP

--- optional-04-oop/Inventory.js
+++ optional-04-oop/Inventory.js
@@ -6,8 +6,25 @@
  * - Memiliki method `removeItem` untuk menghapus item berdasarkan `id`.
  * - Memiliki method `listItems` untuk mengembalikan string yang merupakan informasi detail barang (dipanggil dari fungs `item.displayDetails()`).
  */
+import Item from './Item.js';
 
-class Inventory {}
+class Inventory {
+   constructor() {
+      this.items = [];
+    }
+  
+    addItem(item) {
+      this.items.push(item);
+    }
+  
+    removeItem(id) {
+      this.items = this.items.filter(item => item.id !== id);
+    }
+  
+    listItems() {
+      return this.items.map(item => item.displayDetails()).join('\n');
+    }
+}
 
 // Jangan hapus kode di bawah ini!
 export default Inventory;
