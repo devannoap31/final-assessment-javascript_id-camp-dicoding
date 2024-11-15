94352bbd677e361639b10353b4abe1565792889c
devannoap318 <devannoap318@gmail.com>
2024-11-15 13:40:49 +0000

    solve recursive

--- optional-05-recursive/factorial.js
+++ optional-05-recursive/factorial.js
@@ -1,4 +1,11 @@
-function factorial(n) {}
+function factorial(n) {
+    // Basis: Faktorial 0 atau 1 adalah 1
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+    // Rekursi: n * faktorial dari (n - 1)
+    return n * factorial(n - 1);
+}
 
 // Jangan hapus kode di bawah ini!
 export default factorial;
--- optional-05-recursive/fibonacci.js
+++ optional-05-recursive/fibonacci.js
@@ -1,4 +1,17 @@
-function fibonacci(n) {}
+function fibonacci(n, sequence = [0]) {
+    if (n === 0) return sequence;
+    if (n === 1) {
+        sequence.push(1);
+        return sequence;
+    }
+    if (sequence.length < n + 1) {
+        const last = sequence[sequence.length - 1] || 0;
+        const secondLast = sequence[sequence.length - 2] || 0;
+        sequence.push(last + secondLast || 1);
+        return fibonacci(n, sequence);
+    }
+    return sequence;
+}
 
 // Jangan hapus kode di bawah ini!
 export default fibonacci;
